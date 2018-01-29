c8b124bf39c0f020187ff4e83e48d604359f05f9
Paul Alexander <paulalexander23@live.co.uk>
2018-01-29 09:21:01 +0000

    Modified disperion_relation.m

--- dispersion_relation.m
+++ dispersion_relation.m
@@ -1,14 +1,15 @@
 
-R = 1; cotbeta = 1; S = 1; AD = 1; AT = 1; AB = 1; AK = 1;
-M = 100;
+R = 1; cotbeta = 1; S = 1; AD = 1; AT = 1; AB = 1; AK = 1.9;
+M = 50;
 
 k = linspace(0,1.5,M);
 c = zeros(M,1);
 
 parfor j = 1:M
-    c(j) = OS_eigs_corrected(k(j),R,cotbeta,S,AD,AT,AB,AK);
+    [~,temp] = OS_eigs_corrected(k(j),R,cotbeta,S,AD,AT,AB,AK);
+    c(j) = sort(temp,[],
 end
 
 plot(k,c);
 xlabel('R')
-ylabel('\alpha')
+ylabel('c')
