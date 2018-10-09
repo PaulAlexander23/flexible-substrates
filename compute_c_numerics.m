9b9f494d078fe719db788ed3407712743053135e
Paul Alexander <paulalexander23@live.co.uk>
2018-10-09 09:26:05 +0100

    Add ability to plot multiple modes.

--- compute_c_numerics.m
+++ compute_c_numerics.m
@@ -1,7 +1,8 @@
 function c = compute_c_numerics(k,R,cotbeta,S,AD,AT,AB,AK)
     
     [~,eigenvalues] = compute_OS_eigs(k,R,cotbeta,S,AD,AT,AB,AK);
-    [~,I] = max(imag(eigenvalues));
-    c = eigenvalues(I);
+    %[~,I] = max(imag(eigenvalues));
+    [~,I] = sort(-imag(eigenvalues));
+    c = eigenvalues(I(1:1));
     
 end
--- plot_dispersion_relation.m
+++ plot_dispersion_relation.m
@@ -1,15 +1,16 @@
 function plot_dispersion_relation(R,cotbeta,S,AD,AT,AB,AK)
     
     M = 100;
-    k = linspace(0,2,M);
-    c = zeros(M,1);
+    k = linspace(0.0001,2,M);
+    c = zeros(M,4);
     
     parfor j = 1:M
-        c(j) = compute_c_numerics(k(j),R,cotbeta,S,AD,AT,AB,AK);
+        c(j,:) = compute_c_numerics(k(j),R,cotbeta,S,AD,AT,AB,AK);
     end
     
     plot(k,imag(c));
     xlabel('$\alpha$','Interpreter','latex')
     ylabel('$\Im(c)$','Interpreter','latex')
     title('Dispersion relation')
+    axis([0,2,-2,2])
 end
