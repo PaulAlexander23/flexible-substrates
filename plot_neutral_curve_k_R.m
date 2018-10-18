90fa8951effc0ecde81293aee6924648d4f1028b
Paul Alexander <paulalexander23@live.co.uk>
2018-10-18 13:32:06 +0100

    Add ability to plot neutral curves for multiple modes.

--- plot_neutral_curve_k_R.m
+++ plot_neutral_curve_k_R.m
@@ -1,18 +1,25 @@
 function [ci, R, k] = plot_neutral_curve_k_R(cotbeta,S,AD,AT,AB,AK)
     %PLOT_NEUTRAL_CURVE_K_R
     kN = 100;
-    k = linspace(0,1.5,kN);
+    kL = 5;
+    k = linspace(kL/kN,kL,kN);
     RN = 100;
-    R = linspace(0,5,RN);
-    ci = zeros(kN,RN);
+    RL = 10;
+    R = linspace(RL/RN,RL,RN);
+    numberOfModes = 3;
+    ci = zeros(kN,RN,numberOfModes);
     
     parfor j = 1:RN
         for n = 1:kN
-            ci(n,j) = max(imag(compute_OS_eigs(k(n),R(j),cotbeta,S,AD,AT,AB,AK)));
+            val = imag(compute_OS_eigs(k(n),R(j),cotbeta,S,AD,AT,AB,AK));
+            ci(n,j,:) = val(1:numberOfModes);
+            
         end
     end
-    
-    contour_single(R, k, ci);
+    hold on;
+    for n = 1:numberOfModes
+        contour_single(R, k, ci(:,:,n));
+    end
     xlabel('R');
     ylabel('k');
 end
