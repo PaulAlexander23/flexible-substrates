f6a98fa5bfe967b957e0b30c945042429b14e156
Paul Alexander <paulalexander23@live.co.uk>
2018-11-12 11:00:53 +0000

    Fix vector size mismatch with more than one mode.

--- compute_c_switchboard.m
+++ compute_c_switchboard.m
@@ -7,13 +7,14 @@ function out = compute_c_switchboard(method, k,R,cotbeta,S,AD,AT,AB,AK,AI,modes)
     out = nan(modes,1);
     if (method == "numerical") || (method == "n")
         vec = compute_OS_eigs(k,R,cotbeta,S,AD,AT,AB,AK,AI);
-        out(1:min(modes,length(vec))) = vec;
+        out(1:min(modes,length(vec))) = vec(1:min(modes,length(vec)));
     elseif (method == "longwave") || (method == "l")
         out(1) = compute_long_wave(k,R,cotbeta,0,0,0,0,AK,0);
         %out =
         %compute_long_wave(k,R,cotbeta,S*(k.^2),AD*(k),AT*(k.^2),AB*(k.^4),AK,AI*(k.^2));
         % ^^ rescaled
     elseif (method == "zeroreynolds") || (method == "z")
-        out(1:min(5,modes)) = compute_zero_reynolds(k,cotbeta,S,AD,AT,AB,AK,AI);
+        vec = compute_zero_reynolds(k,cotbeta,S,AD,AT,AB,AK,AI);
+        out(1:min(5,modes)) = vec(1:min(5,modes));
     end
 end
