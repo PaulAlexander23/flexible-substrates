e66f7c404f2da8a94650bee5b4a1c9e4255f46c6
Paul Alexander <paulalexander23@live.co.uk>
2019-07-04 11:59:32 +0100

    Implement inBoth.

---
+++ inBoth.m
@@ -0,0 +1,18 @@
+function c = inBoth(a, b, varargin)
+    p = inputParser;
+    addOptional(p, 'AbsTol', 1e-14, @isnumeric);
+    addOptional(p, 'RelTol', 1e-14, @isnumeric);
+    parse(p, varargin{:});
+
+    aIndex = false(length(a), 1);
+    for j = 1:length(a)
+        location = abs(a(j)-b) <= max(p.Results.RelTol*abs(b), p.Results.AbsTol);
+        aIndex(j) = any(location);
+        if aIndex(j)
+            % Remove first match
+            b(find(location, 1)) = [];
+        end
+    end
+
+    c = a(aIndex);
+end
