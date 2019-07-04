54ec56ffd320056f577a7589ea1958a7d082fce5
Paul Alexander <paulalexander23@live.co.uk>
2019-07-04 14:05:25 +0100

    Add comparision to larger poly count sol, and add match and no match output

--- compute_numerical.m
+++ compute_numerical.m
@@ -4,4 +4,14 @@ function [val, vec, residual] = compute_numerical(k,R,cotbeta,S,AD,AT,AB,AK,AI,n
     end
     
     [val, vec, residual] = compute_OS_eigs(k,R,cotbeta,S,AD,AT,AB,AK,AI,numberOfPolynomials);
+    [val2, ~, ~] = compute_OS_eigs(k,R,cotbeta,S,AD,AT,AB,AK,AI,numberOfPolynomials + 1);
+    
+    [val, index] = inBoth(val,val2,'AbsTol',1e-3,'RelTol',1e-3);
+    
+    if ~isempty(val)
+        vec = vec(:,index);
+        residual = residual(index);
+    else
+        val = nan(1);
+    end
 end
