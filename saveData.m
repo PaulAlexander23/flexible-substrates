30466388432b8abb9dd3f1f447dc6239a2bb73a6
Paul Alexander <paulalexander23@live.co.uk>
2019-07-09 16:28:33 +0100

    Make save data work and specific to this project.

--- saveData.m
+++ saveData.m
@@ -1,4 +1,4 @@
-function saveData(prefix, variablesToSave, varargin)
-    filename = makeFilename(prefix, varargin{:});
-    save(filename, variablesToSave{:});
+function saveData(prefix, c, k, R, cotbeta, S, AD, AT, AB, AK, AI, modes, numberOfPolynomials)
+    filename = makeFilename(prefix, 'cotbeta', cotbeta, 'S', S, 'AD', AD, 'AT', AT, 'AB', AB, 'AK', AK, 'AI', AI, 'modes', modes, 'nop', numberOfPolynomials);
+    save(filename, 'c', 'k', 'R', 'cotbeta', 'S', 'AD', 'AT', 'AB', 'AK', 'AI', 'modes', 'numberOfPolynomials');
 end
