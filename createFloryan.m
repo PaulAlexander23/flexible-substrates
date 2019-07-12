28e68c1462c5d8d0310d63fc62aed52e21b7ed25
Paul Alexander <paulalexander23@live.co.uk>
2019-07-12 11:29:58 +0100

    Plot neutral curves with fixed kapitza number.

---
+++ createFloryan.m
@@ -0,0 +1,15 @@
+function createFloryan(k, R, cotbeta, kaptiza, AD, AT, AB, AK, AI, modes, numberOfPolynomials)
+    
+    Rl = length(R);
+    c = zeros(Rl,length(k),modes);
+    
+    parfor ki = 1:length(k)
+        for Ri = 1:Rl
+            S = kaptiza * R(Ri)^(2/3) * (3/2 * sin(acot(cotbeta)))^(-1/3);
+            c(Ri,ki,:) = imag(compute_c_switchboard('n', k(ki), R(Ri), cotbeta, S, AD, AT, AB, AK, AI, modes, numberOfPolynomials));
+        end
+    end
+    
+    filename = makeFilename("neutral-curve", 'cotbeta', cotbeta, 'kapitza', kaptiza, 'AD', AD, 'AT', AT, 'AB', AB, 'AK', AK, 'AI', AI, 'modes', modes, 'nop', numberOfPolynomials);
+    save(filename, 'c', 'k', 'R', 'cotbeta', 'kaptiza', 'AD', 'AT', 'AB', 'AK', 'AI', 'modes', 'numberOfPolynomials');
+end
