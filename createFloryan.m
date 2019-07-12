ffd8d0edfdcdfe1aed7a7293860cb8ec3dff1141
Paul Alexander <paulalexander23@live.co.uk>
2019-07-12 11:54:15 +0100

    Fix bug and spelling.

--- createFloryan.m
+++ createFloryan.m
@@ -1,15 +1,15 @@
-function createFloryan(k, R, cotbeta, kaptiza, AD, AT, AB, AK, AI, modes, numberOfPolynomials)
+function createFloryan(k, R, cotbeta, kapitza, AD, AT, AB, AK, AI, modes, numberOfPolynomials)
     
     Rl = length(R);
     c = zeros(Rl,length(k),modes);
     
     parfor ki = 1:length(k)
         for Ri = 1:Rl
-            S = kaptiza * R(Ri)^(2/3) * (3/2 * sin(acot(cotbeta)))^(-1/3);
+            S = kapitza * R(Ri)^(-2/3) * (3/2 * sin(acot(cotbeta)))^(-1/3);
             c(Ri,ki,:) = imag(compute_c_switchboard('n', k(ki), R(Ri), cotbeta, S, AD, AT, AB, AK, AI, modes, numberOfPolynomials));
         end
     end
     
-    filename = makeFilename("neutral-curve", 'cotbeta', cotbeta, 'kapitza', kaptiza, 'AD', AD, 'AT', AT, 'AB', AB, 'AK', AK, 'AI', AI, 'modes', modes, 'nop', numberOfPolynomials);
-    save(filename, 'c', 'k', 'R', 'cotbeta', 'kaptiza', 'AD', 'AT', 'AB', 'AK', 'AI', 'modes', 'numberOfPolynomials');
+    filename = makeFilename("neutral-curve", 'cotbeta', cotbeta, 'kapitza', kapitza, 'AD', AD, 'AT', AT, 'AB', AB, 'AK', AK, 'AI', AI, 'modes', modes, 'nop', numberOfPolynomials);
+    save(filename, 'c', 'k', 'R', 'cotbeta', 'kapitza', 'AD', 'AT', 'AB', 'AK', 'AI', 'modes', 'numberOfPolynomials');
 end
