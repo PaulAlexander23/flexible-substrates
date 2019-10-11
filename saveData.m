f0748d06251126af4f288338d429d40cbfad90cc
Paul Alexander <paulalexander23@live.co.uk>
2019-10-11 09:51:05 +0100

    Remove type from file name.

--- saveData.m
+++ saveData.m
@@ -1,4 +1,4 @@
 function saveData(prefix, c, type, k, R, cotbeta, S, AD, AT, AB, AK, AI, modes, numberOfPolynomials)
-    filename = makeFilename(prefix, 'cotbeta', cotbeta, 'S', S, 'AD', AD, 'AT', AT, 'AB', AB, 'AK', AK, 'AI', AI, 'modes', modes, 'nop', numberOfPolynomials, 'type', type);
+    filename = makeFilename(prefix, 'cotbeta', cotbeta, 'S', S, 'AD', AD, 'AT', AT, 'AB', AB, 'AK', AK, 'AI', AI, 'modes', modes, 'nop', numberOfPolynomials);
     save(filename, 'c', 'k', 'R', 'cotbeta', 'S', 'AD', 'AT', 'AB', 'AK', 'AI', 'modes', 'numberOfPolynomials','type');
 end
