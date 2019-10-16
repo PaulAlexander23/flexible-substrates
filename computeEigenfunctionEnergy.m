4e7866f898062ab4597515eff7e2288a36a95e75
Paul Alexander <paulalexander23@live.co.uk>
2019-10-16 11:34:48 +0100

    Rearrange arguments.

--- computeEigenfunctionEnergy.m
+++ computeEigenfunctionEnergy.m
@@ -1,4 +1,4 @@
-function energy = computeEigenfunctionEnergy(k, z, phi)
+function energy = computeEigenfunctionEnergy(z, phi, k)
     u = computeU(z, phi);
     v = computeV(k, phi);
     energy = 0.5 * (u.^2 + v.^2);
