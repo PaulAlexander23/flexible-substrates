c899025d9d5044b31375875e109a214d4b0a2fe4
Paul Alexander <paulalexander23@live.co.uk>
2019-11-21 17:11:33 +0000

    Remove surplus energy decomposition functions

--- computeFluidTerm2.m
+++
@@ -1,9 +0,0 @@
-function term2 = computeFluidTerm2(z, phi, params)
-    dz = diff(z);
-    mz = (z(2:end) + z(1:end-1))/2;
-    
-    mphi = (phi(2:end) + phi(1:end-1))/2;
-    dphidz = diff(phi)./dz;
-    
-    term2 = -8*pi*params.R * sum((imag(mphi.*conj(dphidz)).*(1-mz)) .* dz);
-end
--- computeWallTerm1.m
+++
@@ -1,3 +0,0 @@
-function term1 = computeWallTerm1(k, c, eta, params)
-    term1 = -4*pi*k*params.AD*abs(c)^2*abs(eta)^2;
-end
