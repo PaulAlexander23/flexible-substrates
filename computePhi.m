b0a133a4a0acde282dfd999e96b2c0091685fa59
Paul Alexander <paulalexander23@live.co.uk>
2019-11-27 11:23:15 +0000

    Simplify compute phi

--- computePhi.m
+++ computePhi.m
@@ -1,16 +1,5 @@
 function phi = computePhi(z,vec)
-   %COMPUTE_PHI
-%    N = length(vec)/2 - 3;
-   N = length(vec);
-   T = zeros(length(z),N);%+3);
+   y = -1 + 2*z; % Transform from 0 to 1 into -1 to 1
    
-   y = -1 + 2*z; % Transform to -1 to 1
-   
-   for k = 0:N-1%+2
-       T(:,k+1) = myChebyshevT(k,y);
-   end
-   
-%    phi = T*vec(1:end/2,:);
-
-   phi = T*vec;
+   phi = inverseChebyshevTransform(vec, y);
 end
